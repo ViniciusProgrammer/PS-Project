b877729821996d4183b035c3265293fff80fe1a5
Thereza Angélica <14thereza@gmail.com>
2026-04-11 17:08:25 -0300

    Inicializando aplicação

---
+++ Duna/duna/src/main/java/com/group/duna/DunaApplication.java
@@ -0,0 +1,13 @@
+package com.group.duna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunaApplication.class, args);
+	}
+
+}
---
+++ Duna/duna/src/test/java/com/group/duna/DunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.duna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
