0434d944544a385ac570d93aa3929416f4571a8f
Thereza Angélica <14thereza@gmail.com>
2026-04-11 23:39:58 -0300

    adicionando models de usuário e evento

---
+++ Duna/duna/src/main/java/com/group/duna/model/Evento.java
@@ -0,0 +1,9 @@
+package com.group.duna.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Evento {
+
+    //nome, dataInicio, local, tipo, descrição
+}
---
+++ Duna/duna/src/main/java/com/group/duna/model/Usuario.java
@@ -0,0 +1,13 @@
+package com.group.duna.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Usuario {
+    
+    private String name;
+    private String email;
+    private String passwd;
+
+
+}
